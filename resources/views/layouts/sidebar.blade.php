610ab189d09c2a83d86b50fe56a5b1b4795ee503
rifan rifai <rifanwinprof@gmail.com>
2018-11-25 07:45:48 +0800

    update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,7 +5,7 @@
                 <li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu font-large-1"></i></a></li>
                 <li class="nav-item">
                     <a class="navbar-brand" href="index.html">
-                        <img class="brand-logo" alt="robust admin logo" src="{{asset('qrputih.png')}}">
+                        <img class="brand-logo" alt="robust admin logo" src="{{asset('../../../app-assets/images/logo/logo-dark-sm.png')}}">
                         <h3 class="brand-text">QR Asset</h3>
                     </a>
                 </li>
